5d62138cf62391c27f20631e03ccc5e27abe7fbc
SANTIAGO EITAN SOLMESKY <48458614@ort>
2024-05-06 08:39:51 -0300

    Creación de los metodos en la clase ticketera

--- ticketera.cs
+++ ticketera.cs
@@ -1,6 +1,29 @@
 class Ticketera
 {
-    private DicCliente<int,Cliente>
-    private int UltimoIdEntrada{get;set;}
+    private Dictionary<int,Cliente> DicCliente = new dictionary<int,Cliente>();
+    private int UltimoIdEntrada{get;set;} = 0;
+    public int DevolverUltimoId()
+    {
+
+    }
+
+    public int AgregarCliente(Cliente cliente)
+    {
+
+    }
+
+    public Cliente BuscarCliente(int id)
+    {
+
+    }
+    public int CambiarEntrada(int id, int tipo, int cantidad)
+    {
+
+    }
+    public List <string> estadisticasTicketera()
+    {
+
+    } 
+
 
 }
